a8063b0a8ee6ebe4a2d8d9b5ace64f83ad59fa08
T8RIN <oolbp@bk.ru>
2022-01-16 15:04:52 +0300

    Initial commit

---
+++ app/src/main/java/ru/tech/pokedexcompose/MainActivity.kt
@@ -0,0 +1,42 @@
+package ru.tech.pokedexcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.tech.pokedexcompose.ui.theme.PokedexComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokedexComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PokedexComposeTheme {
+        Greeting("Android")
+    }
+}
