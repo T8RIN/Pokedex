80d0bf00d7aab11e42f5e625acae0ffd571e79dd
T8RIN <oolbp@bk.ru>
2022-02-13 19:40:05 +0300

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0-alpha01")
+        classpath("com.android.tools.build:gradle:7.3.0-alpha02")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
     }
